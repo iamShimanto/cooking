83c7d9776c2a90081bb7306908aa8bd6b7231f57
shimanto-Web-Dev <shimantowebdeveloper@gmail.com>
2024-11-24 21:28:50 +0600

    recipe

---
+++ src/Pages/Home/Common/CommonRecipe.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { PiTimerFill } from "react-icons/pi";
+import { PiForkKnifeFill } from "react-icons/pi";
+
+const CommonRecipe = ({image}) => {
+  return (
+    <>
+      <div className='relative'>
+              <img src="images/food-bg.png" alt="bg" />
+              <div className='absolute bottom-[168px] left-[16px]'>
+                {image}
+              </div>
+              <h2 className='w-[352px] text-[24px] font-semibold font-inter text-black leading-[32px] tracking-[-4%] absolute bottom-[80px] left-[24px]'>Big and Juicy Wagyu Beef Cheeseburger</h2>
+              <div className='flex gap-6 absolute bottom-[32px] left-[24px]'>
+                <div className='flex items-center gap-[10px]'>
+                  <PiTimerFill/>
+                  <p className='text-sm font-medium font-inter text-black tracking-[-2%] opacity-[60%]'>30 Minutes</p>
+                </div>
+                <div className='flex items-center gap-[10px]'>
+                  <PiForkKnifeFill/>
+                  <p className='text-sm font-medium font-inter text-black tracking-[-2%] opacity-[60%]'>Snack</p>
+                </div>
+              </div>
+            </div>
+    </>
+  )
+}
+
+export default CommonRecipe
--- src/Pages/Home/Components/Recipes.jsx
+++ src/Pages/Home/Components/Recipes.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import CommonRecipe from '../Common/CommonRecipe';
 
 const Recipes = () => {
   return (
     <>
-      
+      <section className='pt-[183px]'>
+        <div className="container">
+          <div className="row">
+            <h2 className='text-[48px] font-inter font-semibold text-black tracking-[-4%] text-center'>Simple and tasty recipes</h2>
+            <p className='w-[706px] text-base font-normal font-inter text-black leading-[28px] mt-[24px] text-center mx-auto opacity-[60%]'>Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ad minim </p>
+            <div>
+              
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   )
 }
