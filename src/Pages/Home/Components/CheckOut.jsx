87e1d026528bdf21462f461d62b14e3371d61346
shimanto-Web-Dev <shimantowebdeveloper@gmail.com>
2024-11-25 20:44:38 +0600

    checkout

--- src/Pages/Home/Components/CheckOut.jsx
+++ src/Pages/Home/Components/CheckOut.jsx
@@ -1,33 +1,30 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { FaInstagram } from "react-icons/fa6";
+
+
 
 const CheckOut = () => {
   return (
     <>
-      <section className='mt-[160px] bg-[#E7F9FD]'>
+      <section className='mt-[160px] bg-[#E7F9FD] pb-[80px]'>
         <div className="container">
           <div className="row">
-            <h2 className='mt-[80px] text-[48px] font-semibold font-inter text-black tracking-[-4%] text-center'>Check out @foodieland on Instagram</h2>
+            <h2 className='mt-[80px] text-[48px] font-semibold font-inter text-black tracking-[-4%] text-center pt-[80px]'>Check out @foodieland on Instagram</h2>
             <p className="w-[840px] text-base font-normal font-inter text-black leading-[28px] opacity-[60%] text-center mt-[24px] mx-auto">Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ad minim </p>
-          </div>
-
-          {/* ====================start==================== */}
-              <div>
-                {/* ===============head================ */}
-                <div className='flex gap-[7.73px]'>
-                  <img className='w-[33.75px]' src="images/Oval.png" alt="logo" />
-                  <div>
-                    <Link className='text-[10px] font-inter font-semibold text-[#262626]'>Foodieland.</Link>
-                    <p className='text-[8.51px] font-normal font-inter text-[#262626] tracking-[0.05px] mt-[0.69px]'>Tokyo , Japan</p>
-                  </div>
-                </div>
-                {/* ===============head================ */}
-                <div className="img">
-                  <img src="images/insta-img.png" alt="insta" />
-                </div>
+            <div className="checkout flex justify-evenly mt-[80px]">
+              <img src="images/Post1.png" alt="post" />
+              <img src="images/Post2.png" alt="post" />
+              <img src="images/Post3.png" alt="post" />
+              <img src="images/Post4.png" alt="post" />
+            </div>
+            <div className='flex items-center gap-[16.96px] w-[234px] h-[60px] rounded-[15px] bg-black px-[32px] py-[19px] mx-auto mt-[80.56px] text-white hover:bg-[#CB9DF0] hover:text-black duration-[0.4s]'>
+              <div className='text-sm font-inter font-semibold'>
+                  <Link>Visit Our Instagram</Link>
               </div>
-
-          {/* ====================end ======================= */}
+              <FaInstagram className='text-[22px] '/>
+            </div>
+          </div>
         </div>
       </section>
     </>
